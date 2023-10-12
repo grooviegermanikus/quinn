b177311e54b4bf98c4717bd1f3e4e987720e91b6
GroovieGermanikus <groovie@mango.markets>
2023-10-12 09:56:41 +0200

    randomize content

--- quinn-proto/src/bin/quicping.rs
+++ quinn-proto/src/bin/quicping.rs
@@ -1,13 +1,22 @@
 use std::{cmp::Ordering, io, ops::Range, str};
+use std::net::SocketAddr;
 
 use bytes::{Buf, BufMut, Bytes, BytesMut};
 use thiserror::Error;
-use quinn_proto::pingo::pingo;
+use quinn_proto::pingo::quicping;
 
 
 pub fn main() {
 
-    pingo();
+    let server_addr: SocketAddr = "127.0.0.1:1033".parse().unwrap();
+    // Mango Validator mainnet
+    // let server_addr: SocketAddr = "202.8.9.108:8009".parse().unwrap();
+    // testnet random https://www.validators.app/validators/9cZua5prTSEfednQQc9RkEPpbKDCh1AwnTzv3hE1eq3i?locale=en&network=testnet
+    // let server_addr: SocketAddr = "81.16.237.158:8010".parse().unwrap();
+
+
+    let duration = quicping(server_addr, None);
+    println!("quic-ping to {}: {:.3}ms", server_addr, duration.as_secs_f64() * 1000.0);
 
 
 }
