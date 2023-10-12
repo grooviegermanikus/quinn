4f4d7e6ca8a20e6f6c1ed0929ff2bcda3a0a6379
GroovieGermanikus <groovie@mango.markets>
2023-10-12 08:59:14 +0200

    reset packet.rs

--- quinn-proto/src/packet.rs
+++ quinn-proto/src/packet.rs
@@ -803,8 +803,6 @@ mod tests {
     use crate::DEFAULT_SUPPORTED_VERSIONS;
     use hex_literal::hex;
     use std::io;
-    use std::net::{SocketAddr, UdpSocket};
-    use std::time::{Duration, Instant};
 
     fn check_pn(typed: PacketNumber, encoded: &[u8]) {
         let mut buf = Vec::new();
@@ -902,96 +900,4 @@ mod tests {
             }
         }
     }
-
-    // wireshark: udp.port == 1033 and udp.srcport!=8000 and udp.dstport!=8000
-    #[cfg(feature = "rustls")]
-    #[test]
-    fn pingo() {
-        use crate::{crypto::rustls::initial_keys, Side};
-        use rustls::quic::Version;
-
-        let supported_version = 0xbabababa;
-
-        let dcid = ConnectionId::new(&hex!("06b858ec6f80452b"));
-        let client = initial_keys(Version::V1, &dcid, Side::Client);
-        let mut buf = Vec::new();
-        let header = Header::Long {
-            ty: LongType::Handshake,
-            number: PacketNumber::U8(2), // pn = packet number
-            src_cid: ConnectionId::new(&[]),
-            dst_cid: dcid,
-            version: supported_version,
-        };
-        let encode = header.encode(&mut buf);
-        // let header_len = buf.len();
-        // buf.resize(header_len + 16 + client.packet.local.tag_len(), 0);
-        // pad to 1200 according to https://www.rfc-editor.org/rfc/rfc9000.html#name-initial-packet
-        buf.resize(1200, 0);
-        encode.finish(
-            &mut buf,
-            &*client.header.local,
-            Some((0, &*client.packet.local)),
-        );
-
-        // TODO we should get c3
-        // should be c3babababa12d71a27b19f471ace2e1a5b087003094242b210258c2e2de85341104ea3e426cc34164800449400000002
-        println!("packet");
-        for byte in &buf {
-            print!("{:02x}", byte);
-        }
-        println!();
-        assert_eq!(buf.len(), 1200);
-
-        let socket = UdpSocket::bind("0.0.0.0:0").expect("couldn't bind to address");
-        let server_addr: SocketAddr = "127.0.0.1:1033".parse().unwrap();
-        // Mango Validator mainnet
-        // let server_addr: SocketAddr = "202.8.9.108:8009".parse().unwrap();
-        // testnet random https://www.validators.app/validators/9cZua5prTSEfednQQc9RkEPpbKDCh1AwnTzv3hE1eq3i?locale=en&network=testnet
-        // let server_addr: SocketAddr = "81.16.237.158:8010".parse().unwrap();
-
-        let start_ts = Instant::now();
-        socket.send_to(&buf, server_addr).expect("couldn't send data");
-
-        let mut recv_buf = [0; 1000];
-        socket.set_read_timeout(Some(Duration::from_millis(200))).unwrap();
-        let recv_result = socket.recv(&mut recv_buf);
-        let elapsed = start_ts.elapsed();
-        match recv_result {
-            Ok(received) => println!("received {received} bytes {:?}", &buf[..received]),
-            // on timeout: Os { code: 35, kind: WouldBlock, message: "Resource temporarily unavailable" }
-            Err(e) => println!("recv function failed: {e:?}"),
-        }
-
-        println!("response:");
-        for byte in &recv_buf {
-            print!("{:02x}", byte);
-        }
-        println!();
-
-        let supported_versions = vec![supported_version];
-
-        let decode = PartialDecode::new(buf.as_slice().into(), 0, &supported_versions, false)
-            .unwrap()
-            .0;
-        let mut packet = decode.finish(Some(&*client.header.remote)).unwrap();
-
-        match packet.header {
-            Header::Long { .. } => {
-                println!("packet: {}", packet.header.dst_cid());
-                assert_eq!(&dcid, packet.header.dst_cid());
-            }
-            _ => { panic!("we want long header"); }
-        }
-
-
-        // to testnet:
-        // ICMP 57.503 ms
-        // QUIC 63967us
-
-        // to localhost
-        // 124us
-        // 176us
-        println!("quic-ping to {} to {}us", server_addr, elapsed.as_micros());
-    }
 }
-
