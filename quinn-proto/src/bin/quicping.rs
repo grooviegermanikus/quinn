3be15e451d74986975d3d97dbc5af78c8bc406a4
GroovieGermanikus <groovie@mango.markets>
2024-04-19 18:24:39 +0200

    improve logging and return value

--- quinn-proto/src/bin/quicping.rs
+++ quinn-proto/src/bin/quicping.rs
@@ -8,15 +8,14 @@ use quinn_proto::pingo::quicping;
 
 pub fn main() {
 
-    let server_addr: SocketAddr = "127.0.0.1:1033".parse().unwrap();
+    // let server_addr: SocketAddr = "127.0.0.1:1033".parse().unwrap();
     // Mango Validator mainnet
-    // let server_addr: SocketAddr = "202.8.9.108:8009".parse().unwrap();
+    let server_addr: SocketAddr = "202.8.9.108:8009".parse().unwrap();
     // testnet random https://www.validators.app/validators/9cZua5prTSEfednQQc9RkEPpbKDCh1AwnTzv3hE1eq3i?locale=en&network=testnet
-    // let server_addr: SocketAddr = "81.16.237.158:8010".parse().unwrap();
+    let server_addr: SocketAddr = "204.16.241.212:8009".parse().unwrap();
 
-
-    let duration = quicping(server_addr, None);
-    println!("quic-ping to {}: {:.3}ms", server_addr, duration.as_secs_f64() * 1000.0);
+    let ping_result = quicping(server_addr, None);
+    println!("quic-ping to {}: {}", server_addr, ping_result);
 
 
 }
--- quinn-proto/src/pingo.rs
+++ quinn-proto/src/pingo.rs
@@ -1,4 +1,5 @@
 use std::{cmp::Ordering, io, ops::Range, str};
+use std::fmt::Display;
 use std::net::{SocketAddr, UdpSocket};
 use std::time::{Duration, Instant};
 
@@ -9,13 +10,36 @@ use thiserror::Error;
 use crate::{coding::{self, BufExt, BufMutExt}, crypto, ConnectionId, RandomConnectionIdGenerator, ConnectionIdGenerator};
 use crate::{crypto::rustls::initial_keys, Side};
 use rustls::quic::{HeaderProtectionKey, Version};
-use tracing::{debug, trace};
+use tracing::{debug, enabled, Level, trace, warn};
 use crate::crypto::{HeaderKey, KeyPair, Keys};
 use crate::packet::{Header, LongType, PacketNumber, PartialDecode};
 
 const SUPPORTED_VERSION: u32 = 0xbabababa;
 
-pub fn quicping(server_addr: SocketAddr, recv_timeout: Option<Duration>) -> Duration {
+#[derive(Clone, Debug)]
+pub enum PingResult {
+    Success(Duration),
+    Timeout,
+    Error,
+}
+
+impl PingResult {
+    pub fn is_success(&self) -> bool {
+        matches!(self, Self::Success(_))
+    }
+}
+
+impl Display for PingResult {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            PingResult::Success(duration) => write!(f, "Success({:.3}ms)", duration.as_secs_f64() * 1000.0),
+            PingResult::Timeout => write!(f, "Timeout"),
+            PingResult::Error => write!(f, "Error"),
+        }
+    }
+}
+
+pub fn quicping(server_addr: SocketAddr, recv_timeout: Option<Duration>) -> PingResult {
 
     let mut cid_generator = RandomConnectionIdGenerator::new(12);
 
@@ -24,10 +48,6 @@ pub fn quicping(server_addr: SocketAddr, recv_timeout: Option<Duration>) -> Dura
     let (buf, client_keys) = build_packet(dcid);
 
     let socket = UdpSocket::bind("0.0.0.0:0").expect("couldn't bind to address");
-    // Mango Validator mainnet
-    // let server_addr: SocketAddr = "202.8.9.108:8009".parse().unwrap();
-    // testnet random https://www.validators.app/validators/9cZua5prTSEfednQQc9RkEPpbKDCh1AwnTzv3hE1eq3i?locale=en&network=testnet
-    // let server_addr: SocketAddr = "81.16.237.158:8010".parse().unwrap();
 
     let start_ts = Instant::now();
     socket.send_to(&buf, server_addr).expect("couldn't send data");
@@ -36,15 +56,25 @@ pub fn quicping(server_addr: SocketAddr, recv_timeout: Option<Duration>) -> Dura
     socket.set_read_timeout(recv_timeout).unwrap();
     let recv_result = socket.recv(&mut recv_buf);
     let elapsed = start_ts.elapsed();
-    match recv_result {
-        Ok(received) => debug!("received {received} bytes {:?}", &buf[..received]),
-        // on timeout: Os { code: 35, kind: WouldBlock, message: "Resource temporarily unavailable" }
-        Err(e) => debug!("recv function failed: {e:?}"),
+
+    if enabled!(Level::TRACE) {
+        trace!("quic response:");
+        for byte in &recv_buf {
+            trace!("{:02x}", byte);
+        }
     }
 
-    trace!("response:");
-    for byte in &recv_buf {
-        trace!("{:02x}", byte);
+    match recv_result {
+        Ok(received) => trace!("received {received} bytes {:?}", &buf[..received]),
+        Err(e) => {
+            // on timeout: Os { code: 35, kind: WouldBlock, message: "Resource temporarily unavailable" }
+            return if e.kind() != io::ErrorKind::WouldBlock {
+                warn!("recv function failed: {e:?}");
+                PingResult::Error
+            } else {
+                PingResult::Timeout
+            }
+        }
     }
 
     let supported_versions = vec![SUPPORTED_VERSION];
@@ -54,13 +84,13 @@ pub fn quicping(server_addr: SocketAddr, recv_timeout: Option<Duration>) -> Dura
         .0;
     let mut packet = decode.finish(Some(&*client_keys.header.remote)).unwrap();
 
-    match packet.header {
-        Header::Long { .. } => {
-            trace!("packet: {}", packet.header.dst_cid());
-            assert_eq!(&dcid, packet.header.dst_cid());
-        }
-        _ => { trace!("we want long header"); }
-    }
+    // match packet.header {
+    //     Header::Long { .. } => {
+    //         trace!("packet: {}", packet.header.dst_cid());
+    //         assert_eq!(&dcid, packet.header.dst_cid());
+    //     }
+    //     _ => { trace!("we want long header"); }
+    // }
 
 
     // to testnet:
@@ -72,7 +102,7 @@ pub fn quicping(server_addr: SocketAddr, recv_timeout: Option<Duration>) -> Dura
     // 176us
     debug!("quic-ping to {} to {:.3}ms", server_addr, elapsed.as_secs_f64() * 1000.0);
 
-    elapsed
+    PingResult::Success(elapsed)
 }
 
 fn build_packet(dcid: ConnectionId) -> (Vec<u8>, Keys) {
